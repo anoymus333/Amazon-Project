93ba145c385e1f646ca7ffe129a06eb305bd21f4
aditya regmi <adityaregmi27@gmail.com>
2026-08-16 09:30:25 +0545

    Delete Products from html while clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
 
 cartSummaryHtml += `
     
- <div class="cart-item-container">
+ <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -101,7 +101,7 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
 
         const productId = link.dataset.productId;
         removeFromCart(productId);
-        
+        document.querySelector(`.js-cart-item-container-${productId}`).remove();
 
     });
 });
